6ec699831b4feee366f09d3aa6614c6f49dd1b66
Akshat Jindal <Akshat.Jindal-e@maf.ae>
2020-08-16 15:01:24 +0530

    Added exception handler when product id is not present

--- product/src/main/java/org/inventorymanagement/product/repository/MongoConnection.java
+++ product/src/main/java/org/inventorymanagement/product/repository/MongoConnection.java
@@ -3,8 +3,10 @@ package org.inventorymanagement.product.repository;
 import org.inventorymanagement.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MongoConnection extends MongoRepository<Product, String> {
 
-    Product findByProductId(String productId);
+    Optional<Product> findByProductId(String productId);
 
 }
---
+++ product/src/main/java/org/inventorymanagement/product/service/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package org.inventorymanagement.product.service;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(){
+        super();
+    }
+}
