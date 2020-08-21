a8f6dcda9bea2bdf83f429ca70b79522c3978a53
Shagun Bandi <shabandi@wkmin2964077.global.publicisgroupe.net>
2020-08-21 14:15:18 +0530

    reorganise

---
+++ front/src/assets/jss/navbarStyle.jsx
@@ -0,0 +1,32 @@
+const drawerWidth = 240;
+
+const useStyles = (theme) => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: "hidden",
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            width: `calc(100% - ${theme.spacing(9) + 1}px)`,
+        },
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(["width", "margin"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 36,
+    },
+    hide: {
+        display: "none",
+    },
+});
+
+export { useStyles };
--- front/src/components/Navigation/index.jsx
+++ front/src/components/Navigation/index.jsx
@@ -1,17 +1,119 @@
-import React, { Component } from "react";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { withStyles } from "@material-ui/core/styles";
+import withWidth from "@material-ui/core/withWidth";
+import MailIcon from "@material-ui/icons/Mail";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import clsx from "clsx";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import MiniDrawer from './Drawer'
 
-export class Navigation extends Component {
+import { toggleDrawer } from "../../store/actions/appAction";
+import companyLogo from '../../assets/images/reactlogo.png';
+import { useStyles } from '../../assets/jss/navigationStyle';
+
+class Navigation extends Component {
+  handleDrawerClose = () => {
+    this.props.toggleDrawer();
+  };
+
+  responsiveDrawer = () => {
+    const { width } = this.props;
+
+    const isSmallScreen = /xs/.test(width);
+    var drawerProps = {
+      variant: isSmallScreen ? "tempporary" : "permanent",
+    };
+    if (isSmallScreen)
+      drawerProps = {
+        open: this.props.isDrawerOpen,
+        onClose: this.handleDrawerClose,
+        ...drawerProps,
+      };
+    return drawerProps;
+  };
+
+  getBrand = () => {
+    const { logoText } = this.props;
+
+    const brand = (
+      <List>
+        <ListItem>
+          <ListItemIcon>
+            <MailIcon />
+          </ListItemIcon>
+          <ListItemText primary={logoText} />
+        </ListItem>
+      </List>
+    );
+    return brand;
+  };
+
   render() {
+    const { classes, isDrawerOpen } = this.props;
     return (
-      <MiniDrawer/>
+      <Fragment>
+        <Drawer
+          {...this.responsiveDrawer()}
+          className={clsx(classes.drawer, {
+            [classes.drawerOpen]: isDrawerOpen,
+            [classes.drawerClose]: !isDrawerOpen,
+          })}
+          classes={{
+            paper: clsx(classes.drawer, {
+              [classes.drawerOpen]: isDrawerOpen,
+              [classes.drawerClose]: !isDrawerOpen,
+            }),
+          }}
+        >
+          {/* <List>
+            {["MY APP"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List> */}
+          {this.getBrand()}
+          <Divider />
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Fragment>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  products: state.product.allProducts,
+  isDrawerOpen: state.app.isDrawerOpen,
+  logoText: state.app.companyName,
 });
 
-export default connect(mapStateToProps, {})(Navigation);
+export default connect(mapStateToProps, { toggleDrawer })(
+  withWidth()(withStyles(useStyles, { withTheme: true })(Navigation))
+);
